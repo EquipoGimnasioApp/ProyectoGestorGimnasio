6731e09e96c983e3af43d466bfa32f4ea140bc37
FPolito98 <francisco_polito98@hotmail.com>
2025-06-29 20:49:47 -0300

    Se creo l apagina del profesor para asignarle rutinas a los alumnos

--- gestor-gimnasio-back/app/Http/Controllers/RutinaController.php
+++ gestor-gimnasio-back/app/Http/Controllers/RutinaController.php
@@ -21,5 +21,29 @@ class RutinaController extends Controller
         return response()->json($rutina, Response::HTTP_OK);
     }
 
+    public function create(Request $request, $usuarioId)
+    {
+        $data = $request->validate([
+            'descripcion' => 'required|string|max:300',
+        ]);
+        $data['id_usuario'] = $usuarioId;
+        $rutina = $this->rutinaService->create($data);
+        return response()->json($rutina, Response::HTTP_CREATED);
+    }
+
+    public function update($idUsuario, Request $request)
+    {
+        $data = $request->validate([
+            'descripcion' => 'required|string|max:300',
+        ]);
+        $rutina = $this->rutinaService->update($idUsuario, $data);
+        return response()->json($rutina, Response::HTTP_OK);
+    }
+
+    public function destroy($id)
+    {
+        $this->rutinaService->delete($id);
+        return response()->json(null, Response::HTTP_NO_CONTENT);
+    }
     // Agrega aquí los métodos create, update, destroy si los necesitas
 }
